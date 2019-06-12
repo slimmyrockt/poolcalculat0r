37f0e99691ba08ce729a9b397ab03bf5c7ec8208
ml029 <marcus.luther@rbb-online.de>
2019-06-12 21:16:54 +0200

    basehref= /pool/

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -2,46 +2,51 @@ import {Component, OnInit} from '@angular/core';
 import {Werte} from '../models/werte';
 
 @Component({
-    selector: 'app-form',
-    templateUrl: './form.component.html',
-    styleUrls: ['./form.component.css']
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
 
-    werte: Werte;
-    content = require('../../assets/mydata.json');
-    ergebnis: string;
+  werte: Werte;
+  content = require('../../assets/mydata.json');
+  ergebnis: string;
 
-    constructor() {
-        this.werte = new Werte();
-        this.werte.poolSize = this.content.poolSize;
-        this.werte.pHWert = this.content.pHWert;
-        this.werte.wirkungsGradGramm = this.content.wirkungsGradGramm;
-        this.werte.wirkungsGradKubikmeter = this.content.wirkungsGradKubikmeter;
-        this.werte.wirkungsGradPHWert = this.content.wirkungsGradPHWert;
-        this.ergebnis = null;
+  constructor() {
+    this.werte = new Werte();
+    this.werte.poolSize = this.content.poolSize;
+    this.werte.pHWert = this.content.pHWert;
+    this.werte.wirkungsGradGramm = this.content.wirkungsGradGramm;
+    this.werte.wirkungsGradKubikmeter = this.content.wirkungsGradKubikmeter;
+    this.werte.wirkungsGradPHWert = this.content.wirkungsGradPHWert;
+    this.ergebnis = null;
+  }
+
+  ngOnInit() {
+    console.log(this.content);
+  }
+
+  berechnen() {
+    //TODO: plus / Minus erkennen und danach berechnen!
+    if (this.werte.pHWert < 7.2 && this.werte.pHWert > 0 && this.werte.wirkungsGradPHWert > 0) {
+      this.ergebnis = Math.round((this.werte.poolSize * this.werte.wirkungsGradGramm / this.werte.wirkungsGradKubikmeter) * (7.2 - this.werte.pHWert) / this.werte.wirkungsGradPHWert) + ' Gramm';
     }
 
-    ngOnInit() {
-        console.log(this.content);
+    if (this.werte.pHWert < 7.2 && this.werte.pHWert > 0 && this.werte.wirkungsGradPHWert < 0) {
+      this.ergebnis = 'Du musst pH-Plus nehmen und den Wirkungsgrad vergleichen!';
     }
 
-    berechnen() {
-        //TODO: plus / Minus erkennen und danach berechnen!
-        if (this.werte.pHWert <= 7.2) {
-            this.ergebnis = 'Nichts hinzugeben!';
-        }
-
-        if (this.werte.pHWert > 7.2) {
-
-            this.ergebnis = Math.round((this.werte.poolSize * this.werte.wirkungsGradGramm / this.werte.wirkungsGradKubikmeter) * (this.werte.pHWert - 7.4) / this.werte.wirkungsGradPHWert) + ' Gramm';
-
-            console.log('Zuviel:' + (this.werte.pHWert - 7.4));
-            console.log('Gramm um den PH-Wert um 0.1 zu senken:' + (this.werte.poolSize * this.werte.wirkungsGradGramm / this.werte.wirkungsGradKubikmeter));
-            console.log(this.werte.wirkungsGradKubikmeter);
-            console.log(this.werte.wirkungsGradGramm);
-            console.log(this.werte.poolSize);
-            console.log((this.werte.poolSize * this.werte.wirkungsGradGramm / this.werte.wirkungsGradKubikmeter) * (this.werte.pHWert - 7.4) / this.werte.wirkungsGradPHWert);
-        }
+    if (this.werte.pHWert > 7.4 && this.werte.wirkungsGradPHWert < 0) {
+      this.ergebnis = Math.round(-1 * (this.werte.poolSize * this.werte.wirkungsGradGramm / this.werte.wirkungsGradKubikmeter) * (this.werte.pHWert - 7.4) / this.werte.wirkungsGradPHWert) + ' Gramm';
     }
+
+    if (this.werte.pHWert > 7.4 && this.werte.wirkungsGradPHWert > 0) {
+      this.ergebnis = 'Du musst pH-Minus nehmen und den Wirkungsgrad vergleichen!';
+    }
+
+    if (this.werte.pHWert >= 7.2 && this.werte.pHWert < 7.4) {
+      this.ergebnis = 'pH-Wert ist super';
+    }
+
+  }
 }
